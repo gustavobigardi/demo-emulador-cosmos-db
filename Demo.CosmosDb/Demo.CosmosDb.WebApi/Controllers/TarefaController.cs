b9faeed5965640da1c0ab8de27139e4ff5c8974f
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2018-10-18 22:56:22 -0300

    Adicionando projeto

---
+++ Demo.CosmosDb/Demo.CosmosDb.WebApi/Controllers/TarefaController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.CosmosDb.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.CosmosDb.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+        private TarefaContexto _contexto;
+
+        public TarefaController(TarefaContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public IActionResult ListTarefas()
+        {
+            return new ObjectResult(_contexto.ListarTarefas<Tarefa>());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTarefa(string id)
+        {
+            Tarefa tarefa = _contexto.ObterIarefa<Tarefa>(id);
+
+            if (tarefa != null)
+                return new ObjectResult(tarefa);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult InsertTarefa(Tarefa tarefa)
+        {
+            _contexto.AdicionarTarefa(tarefa);
+            return Ok();
+        }
+    }
+}
---
+++ Demo.CosmosDb/Demo.CosmosDb.WebApi/Models/TarefaContexto.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.CosmosDb.WebApi.Models
+{
+    public class TarefaContexto
+    {
+        private readonly IConfiguration _configuration;
+
+        public TarefaContexto(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public T ObterIarefa<T>(string codigo)
+        {
+            MongoClient client = new MongoClient(
+                _configuration.GetConnectionString("Default"));
+            IMongoDatabase db = client.GetDatabase("DBTarefas");
+
+            var filter = Builders<T>.Filter.Eq("Codigo", codigo);
+
+            return db.GetCollection<T>("Tarefas")
+                .Find(filter).FirstOrDefault();
+        }
+
+        public IEnumerable<T> ListarTarefas<T>()
+        {
+            MongoClient client = new MongoClient(
+                _configuration.GetConnectionString("Default"));
+            IMongoDatabase db = client.GetDatabase("DBTarefas");
+
+            return db.GetCollection<T>("Tarefas").Find(_ => true).ToList();
+        }
+
+        public void AdicionarTarefa<T>(T tarefa)
+        {
+            MongoClient client = new MongoClient(
+               _configuration.GetConnectionString("Default"));
+            IMongoDatabase db = client.GetDatabase("DBTarefas");
+
+            db.GetCollection<T>("Tarefas").InsertOne(tarefa);
+        }
+    }
+}
